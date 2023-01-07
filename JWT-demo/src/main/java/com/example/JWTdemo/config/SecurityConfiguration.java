64eaa78a0b267bd3d5eb096ef689457623cf9a60
DaRkSoUl1690 <87258454+DaRkSoUl1690@users.noreply.github.com>
2023-01-07 13:03:12 +0530

    Initial commit

---
+++ JWT-demo/src/main/java/com/example/JWTdemo/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.example.JWTdemo.config;
+
+import com.example.JWTdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * The type Security configuration.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+}
